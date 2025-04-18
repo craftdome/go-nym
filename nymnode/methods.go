f91b75b26722eee9d1ee89515d42e105832fda43
Terra Nova <croc.sb9@gmail.com>
2025-04-18 17:10:36 +0500

    embed url endpoint to error

--- nymnode/methods.go
+++ nymnode/methods.go
@@ -45,11 +45,12 @@ func (c *Client) GetHostInformation(ctx context.Context) (HostInformation, error
 }
 
 func (c *Client) GetLoad(ctx context.Context) (Load, error) {
+	endpoint := c.endpoints[EndpointLoad]
 	if c.Version < v150 {
-		return Load{}, errors.Wrapf(ErrUnsupportedNodeMethod, "GetLoad available since %s, current=%s", v150, c.Version)
+		return Load{}, errors.Wrapf(ErrUnsupportedNodeMethod, "%s available since %s, current=%s", endpoint, v150, c.Version)
 	}
 
-	return get[Load](ctx, c.client, c.limiter, c.endpoints[EndpointLoad])
+	return get[Load](ctx, c.client, c.limiter, endpoint)
 }
 
 func (c *Client) GetRoles(ctx context.Context) (Roles, error) {
